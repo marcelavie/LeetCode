88eff76c2f7aed06f6843f9decab98e10a3b98e2
Marcela Lima <marcelalima.df@gmail.com>
2023-02-06 11:59:11 -0300

    ajuste temp

--- Easy/convert_the_temperature.py
+++ Easy/convert_the_temperature.py
@@ -1,8 +1,9 @@
 # Link do exercício: https://leetcode.com/problems/convert-the-temperature/submissions/876821438/
-def convertTemperature(celsius):
-    kelvin = celsius + 273.15
-    fahrenheit = celsius * 1.8 + 32
-    return [kelvin, fahrenheit]
+class Solution(object):
+    def convertTemperature(self, celsius):
+        kelvin = celsius+273.15
+        fahrenheit = celsius*1.8+32
+        return [kelvin, fahrenheit]
 
 
 assert convertTemperature(36.5) == [309.65,97.7] 
