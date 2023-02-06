7225879f4a4b43e09069f8711965ba61d0181159
Marcela Lima <marcelalima.df@gmail.com>
2023-02-06 11:30:05 -0300

    o ajuste quebrou kkk

--- Easy/Integer_to_roman.py
+++ Easy/Integer_to_roman.py
@@ -1,20 +1,17 @@
 # Link do exercício: https://leetcode.com/problems/roman-to-integer/description/
-def romanToInt(s):
-    valores = {'I':1, 'V':5, 'X':10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    total = 0
-    anterior = 0
+class Solution(object):
+    def romanToInt(self, s):
+        dict = {'I':1, 'V':5, 'X':10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        total = 0
+        atual = 0
+        anterior = 0
 
-    for romano in s:
-        valor_atual = valores[romano]
-        if valor_atual > anterior:
-            total = total + valor_atual - 2 * anterior
-        else: 
-            total += valor_atual
+        for i in range(len(s)):
+            atual = dict[s[i]]
+            if atual > anterior:
+                total = total + atual - 2 * anterior
+            else: 
+                total += atual
 
-        anterior = valor_atual
-    return total
-    
-
-assert romanToInt('III') == 3
-assert romanToInt("LVIII") == 58 
-assert romanToInt("MCMXCIV") == 1994
+            anterior = atual
+        return total
